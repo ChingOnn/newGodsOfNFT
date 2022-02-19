63c699e470a6051d88a4b1e767202cc098daec36
chingonn <chingonnng@gmail.com>
2022-02-19 16:23:54 +0800

    add spacing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
@@ -6,16 +5,17 @@ import Button from '@mui/material/Button';
 
 function App() {
   return (
+        
+        <Card class="card">
+          <TextField id="outlined-basic" label="Name" variant="outlined">Name</TextField>   
+          <br></br>
+          <TextField id="outlined-basic" label="Password" variant="outlined">Password</TextField>
+          <br></br>
+          <Button variant="contained">Log in</Button>
+          <br></br>
+          <Button variant="outlined">Register</Button>
+        </Card>
 
-    <Card class="card">
-      <TextField id="outlined-basic" label="Name" variant="outlined">Name</TextField>   
-      <TextField id="outlined-basic" label="Password" variant="outlined">Password</TextField>
-      <Button variant="contained">Log in</Button>
-    </Card>
-    
-
-    
-    
   );
 }
 
